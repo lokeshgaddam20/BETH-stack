c0b973b90d1dd71c0ea2c93d8c6110f0887c61a0
LokeshG <gaddamlokesh20@gmail.com>
2023-11-13 23:35:52 +0530

    index.ts

--- Hypermedia-todo/index.ts
+++ Hypermedia-todo/index.ts
@@ -1,3 +1,4 @@
 import {Elysia} from "elysia";
 
-const app = new Elysia().
+const app = new Elysia().get("/",()=> "Hello").listen(3000);
+console.log(`Server running at http://${app.server?.hostname}:${app.server?.port}`)
